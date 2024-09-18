2c466fc097399e479d8a796548e86f0763082c9f
Avish <avitmr2345@gmail.com>
2024-09-18 12:35:48 +0530

    initialized SpringBoot repo

---
+++ src/test/java/com/myfirst_springapp/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirst_springapp.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
