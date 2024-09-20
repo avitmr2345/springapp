683b9bf7e5d0cb176fb8bd94252a5c60737e637a
Avish <avitmr2345@gmail.com>
2024-09-20 15:45:10 +0530

    injecting custom application properties

--- src/main/java/com/myfirst_springapp/springapp/rest/NewRestController.java
+++ src/main/java/com/myfirst_springapp/springapp/rest/NewRestController.java
@@ -1,11 +1,23 @@
 package com.myfirst_springapp.springapp.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class NewRestController {
 
+    @Value("${owner.name}")
+    public String ownerName;
+    
+    @Value("${owner.surname}")
+    public String ownerSurname;
+
+    @GetMapping("owner")
+    public String getOwnerInfor(){
+        return "Owner: " + ownerName + ", Surname: " + ownerSurname;
+    }
+    
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
