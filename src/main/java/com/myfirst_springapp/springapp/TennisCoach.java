641a8416a02ab5fe8e238d07545eda07a4bc863f
Avish <avitmr2345@gmail.com>
2024-09-22 17:17:20 +0530

    learned bean scope, bean lifecycle methods to add custom code during bean initialization and destruction using PreConstruct and PreDestroy

--- src/main/java/com/myfirst_springapp/springapp/TennisCoach.java
+++ src/main/java/com/myfirst_springapp/springapp/TennisCoach.java
@@ -1,10 +1,8 @@
 package com.myfirst_springapp.springapp;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TennisCoach implements Coach {
 
     public TennisCoach() {
--- src/main/java/com/myfirst_springapp/springapp/rest/DemoController.java
+++ src/main/java/com/myfirst_springapp/springapp/rest/DemoController.java
@@ -1,13 +1,18 @@
 package com.myfirst_springapp.springapp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.myfirst_springapp.springapp.Coach;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotheCoach;
 
     // @Autowired
     // public DemoController(Coach theCoach) {
@@ -15,9 +20,22 @@ public class DemoController {
     // }
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public void setCoach(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotheCoach = theAnotherCoach;
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("Comparing beans: myCoach & anotherCoach, " + (myCoach == anotheCoach));
+    }
+    
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("Bean deleted.");
     }
 
     @GetMapping("/dailyworkout")
