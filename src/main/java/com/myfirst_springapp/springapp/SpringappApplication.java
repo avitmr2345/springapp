23b3c969211c91f06cda6183bf1f9806344398e1
Avish <avitmr2345@gmail.com>
2024-09-23 23:29:09 +0530

    setting up Spring Boot project for hibernate JPA

--- src/main/java/com/myfirst_springapp/springapp/SpringappApplication.java
+++ src/main/java/com/myfirst_springapp/springapp/SpringappApplication.java
@@ -1,13 +1,22 @@
 package com.myfirst_springapp.springapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello");
+		};
 	}
 
 }
