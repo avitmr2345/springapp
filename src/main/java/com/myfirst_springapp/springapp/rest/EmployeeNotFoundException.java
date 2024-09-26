e7141ff8b06e9eb86e1a59b3a807df8ea9f78422
Avish <avitmr2345@gmail.com>
2024-09-26 23:46:48 +0530

    added exception handling for bad path variable that doesn't not exist

---
+++ src/main/java/com/myfirst_springapp/springapp/rest/EmployeeNotFoundException.java
@@ -0,0 +1,17 @@
+package com.myfirst_springapp.springapp.rest;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+
+    public EmployeeNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmployeeNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+}
--- src/main/java/com/myfirst_springapp/springapp/rest/MyRestController.java
+++ src/main/java/com/myfirst_springapp/springapp/rest/MyRestController.java
@@ -3,6 +3,10 @@ package com.myfirst_springapp.springapp.rest;
 import java.util.List;
 import java.util.ArrayList;
 import com.myfirst_springapp.springapp.entity.Employee;
+import com.myfirst_springapp.springapp.entity.EmployeeErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +36,23 @@ public class MyRestController {
     // @PathVariable will link the method parameter to the path variable
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
+
+        if ((employeeId >= theEmployees.size()) || (employeeId < 0)) {
+            throw new EmployeeNotFoundException("Employee id " + employeeId + " not found");
+        }
+
         return theEmployees.get(employeeId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException exception) {
+        EmployeeErrorResponse error = new EmployeeErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
 }
