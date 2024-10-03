3310980ccde8bdbd0c42e93c788dfad78fb4526a
Avish <avitmr2345@gmail.com>
2024-10-03 21:40:34 +0530

    remove whitespaces and if all whitespaces set it to null (problem fixed of NotNull)

--- src/main/java/com/myfirst_springapp/springapp/controller/CustomerController.java
+++ src/main/java/com/myfirst_springapp/springapp/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.myfirst_springapp.springapp.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.myfirst_springapp.springapp.validation.Customer;
@@ -11,6 +14,14 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        // it registers the StringTrimmer to the String class fields
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     // Model allows us to share information b/w Controllers and view
     // pages(Thymeleaf)
