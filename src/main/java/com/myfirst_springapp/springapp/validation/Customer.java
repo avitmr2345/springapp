8ec46a5ff938879d40675d277299ecd98dc84158
Avish <avitmr2345@gmail.com>
2024-10-03 22:22:39 +0530

    validating a number range

--- src/main/java/com/myfirst_springapp/springapp/validation/Customer.java
+++ src/main/java/com/myfirst_springapp/springapp/validation/Customer.java
@@ -1,5 +1,7 @@
 package com.myfirst_springapp.springapp.validation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,6 +13,18 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater or equal to 0")
+    @Max(value = 10, message = "must be less or equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
