baf476ce875eeb8550109ac3c816f74f790aa3ab
Avish <avitmr2345@gmail.com>
2024-10-08 22:00:10 +0530

    displayed user ID and roles

--- demosecurity/src/main/java/com/springsecurity/demosecurity/security/DemoSecurityConfig.java
+++ demosecurity/src/main/java/com/springsecurity/demosecurity/security/DemoSecurityConfig.java
@@ -3,10 +3,35 @@ package com.springsecurity.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails avish = User.builder()
+                .username("avish")
+                .password("{noop}avishtomar")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}avishtomar")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails saurabh = User.builder()
+                .username("saurabh")
+                .password("{noop}avishtomar")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(avish, john, saurabh);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
