4f17eeb0fc31fb58ee794cba2ce4d87797d853a0
Avish <avitmr2345@gmail.com>
2024-12-17 14:50:45 +0530

    Read Method Arguments with JoinPoint implemented

--- aopdemo/src/main/java/com/aop/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/aop/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,9 +1,13 @@
 package com.aop.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import com.aop.aopdemo.Account;
 
 @Aspect
 @Component
@@ -14,8 +18,24 @@ public class MyDemoLoggingAspect {
     };
 
     @Before("forDAOPackage()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n>> Executing @Before advice on addAccount()");
+
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        Object[] args = theJoinPoint.getArgs();
+
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+                Account theAccount = (Account) tempArg;
+                System.out.println("Account name: " + theAccount.getName());
+                System.out.println("Account level: " + theAccount.getLevel());
+            }
+        }
+
     }
 
     @Before("forDAOPackage()")
