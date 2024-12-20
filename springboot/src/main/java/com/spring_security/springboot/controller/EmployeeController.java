da982cde26ae0e29479bc57940a485e58c06f6cf
Avish <avitmr2345@gmail.com>
2024-12-20 20:43:35 +0530

    AOP Logging support on MVC CRUD app implemented

--- springboot/src/main/java/com/spring_security/springboot/controller/EmployeeController.java
+++ springboot/src/main/java/com/spring_security/springboot/controller/EmployeeController.java
@@ -9,7 +9,6 @@ import com.spring_security.springboot.entity.Employee;
 import com.spring_security.springboot.service.EmployeeService;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
