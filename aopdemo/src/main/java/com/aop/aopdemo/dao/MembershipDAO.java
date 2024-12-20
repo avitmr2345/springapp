35a27b4ff4eb6c06d18ee714fa891bf0a328d67a
Avish <avitmr2345@gmail.com>
2024-12-20 20:46:57 +0530

    removed springboot, demosecurity, aopdemo from springapp

--- aopdemo/src/main/java/com/aop/aopdemo/dao/MembershipDAO.java
+++
@@ -1,7 +0,0 @@
-package com.aop.aopdemo.dao;
-
-public interface MembershipDAO {
-    void addAccount();
-
-    void goToSleep();
-}
--- aopdemo/src/main/java/com/aop/aopdemo/service/TrafficFortuneService.java
+++
@@ -1,7 +0,0 @@
-package com.aop.aopdemo.service;
-
-public interface TrafficFortuneService {
-    String getFortune();
-
-    String getFortune(boolean tripWire);
-}
